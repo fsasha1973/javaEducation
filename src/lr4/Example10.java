9223920e206fff6258ea392c7e359e25365861d6
fsasha1973 <r8476@mail.ru>
2022-12-23 21:56:52 +0500

    lr 4

--- src/lr4/Example10.java
+++ src/lr4/Example10.java
@@ -3,34 +3,33 @@ package lr4;
 import java.util.Scanner;
 public class Example10 {
     public static void main(String[] args) {
-        Scanner id = new Scanner(System.in);// Создание объекта класса Scanner
-        // При запуске программы в консоль необходимо вывести сообщение: «Введите текст для шифрования»,
+        Scanner id = new Scanner(System.in);
+
         System.out.print("Введите текст для шифрования: ");
         String text = id.nextLine();
-        // после ввода текста, появляется сообщение: «Введите ключ».
-        System.out.print("Введите ключ: ");// пользоватьелю
+
+        System.out.print("Введите ключ: ");
         int shift = id.nextInt();
         String encryptStr = TooEncript.encrypt(text, shift);
-        // После того как введены все данные, необходимо вывести преобразованную строку с сообщением «Текст после преобразования : ».
+
         System.out.print("Текст после преобразования :" + "\"" + encryptStr + "\"");
-        // Далее необходимо задать вопрос пользователю: «Выполнить обратное преобразование? (y/n)»,
-        System.out.print("\nВыполнить обратное преобразование? (y/n)"); // пользователю
+
+        System.out.print("\nВыполнить обратное преобразование? (y/n)");
         boolean cool = false;
         while (!cool) {
             String reply = id.next();
-            // если пользователь вводит «y», тогда выполнить обратное преобразование.
+
             if (reply.toLowerCase().equals("y")) {
                 String decryptStr = TooEncript.decrypt(encryptStr, shift);
                 System.out.println(decryptStr);
                 cool = true;
             }
-            // Если пользователь вводит «n», того программа выводит сообщение «До свидания!».
+
             else if (reply.toLowerCase().equals("n")) {
                 System.out.println("До свидания!");
                 cool = true;
             }
-            // Если пользователь вводит что-то другое, отличное от «y» или «n»,
-            // то программа ему выводит сообщение: «Введите корректный ответ».
+
             else {
                 System.out.println("Введите корректный ответ: ");
                 cool = false;
